1b8a36e555866e77da65f03e280f0fc6e54e34c9
MalashenkoVitalii <malashenko228o@gmail.com>
2021-09-11 12:47:04 +0300

    Forst commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    var mixer = mixitup('.portfolio__content');
+
+    $('.menu__btn').on('click', function(){
+        $('.menu_list').toggleClass('menu_list--active');
+    });
+    // var mixer = mixitup('.portfolio__content');
+});
+
+
